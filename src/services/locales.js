31ef1c5f884e1feb4f9b96f4a22666244363370a
JoseDanielNovaMuñozCAMPUS <josedanielnova2027@gmail.com>
2023-08-15 15:29:00 -0500

    feat: :sparkles: Enpoint de locales finalizado
    termine el endpoint de locales que hace GET,POST,PUT,DELETE de la coleccion locales

--- src/services/locales.js
+++ src/services/locales.js
@@ -1,9 +1,26 @@
+import { ObjectId } from 'mongodb';
 import db from '../config/mongodb.js';
 const locales = db.getInstance().changeCollection('locales').connect()
 
 export default class Locales {
     static async postLocal(req, res) {
-        await locales.find();
-        res.send(":3")
+        let consulta = await locales.insertOne(req.body)
+        res.status(200).json(consulta)
+    }
+    static async getLocal(req, res) {
+        let consulta = await locales.find({}).toArray()
+        res.status(200).json(consulta)
+    }
+    static async putLocal(req, res) {
+        let consulta = await locales.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body })
+        res.status(200).json(consulta)
+    }
+    static async deleteLocal(req, res) {
+        let consulta = await locales.deleteOne({ _id: new ObjectId(req.params.id) })
+        res.status(200).json(consulta)
+    }
+    static async getLocalById(req, res) {
+        let consulta = await locales.findOne({ _id: new ObjectId(req.params.id) })
+        res.status(200).json(consulta)
     }
 }
