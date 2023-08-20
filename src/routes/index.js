618cadf83cca75175b5c1ae94f4e33aee64aad59
Juan diego <juandiegoramirez098@gmail.com>
2023-08-20 18:10:34 -0500

    fix: :bug: se aregglo el bug de barra en windowa

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,39 @@
 import { Router } from "express";
 import { readdirSync } from "fs";
 import path from 'path';
+import os from "os";
 
-const PATH_ROUTERS = path.dirname(new URL(import.meta.url).pathname);
+const platform = os.platform();
+let PATH_ROUTERS;
+if(platform == "win32") PATH_ROUTERS = path.dirname(new URL(import.meta.url).pathname).replace(/^\/(\w\:)/, '$1');
+else PATH_ROUTERS = path.dirname(new URL(import.meta.url).pathname);
 const router = Router();
 
 const cleanFile = (fileName) => {
-  const file = fileName.split(".").shift();
-  return file;
+    const file = fileName.split(".").shift();
+    return file;
 };
 
-const filesNames = readdirSync(PATH_ROUTERS);
+const loadModules = async () => {
+    const filesNames = readdirSync(PATH_ROUTERS);
 
-filesNames.forEach(async (fileName) => {
-    
-  const cleanName = cleanFile(fileName);
-  if (cleanName === "index") return;
-  const moduleRouter = await import(`./${fileName}`);
-  router.use(`/${cleanName}`, moduleRouter.router);
-});
+    const importPromises = filesNames.map(async (fileName) => {
+        const cleanName = cleanFile(fileName);
+        if (cleanName !== "index") {
+            try {
+                const moduleRouter = await import(`./${fileName}`);
+                if (moduleRouter.router) {
+                    router.use(`/${cleanName}`, moduleRouter.router);
+                }
+            } catch (error) {
+                console.error(`Error al cargar el modulo '${fileName}':`, error);
+            }
+        }
+    });
+
+    await Promise.all(importPromises);
+};
+
+loadModules();
 
 export default router;
