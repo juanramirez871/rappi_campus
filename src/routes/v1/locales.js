8eb213c48685ed8913a977dfe92d81c6e056ae48
juan diego <juandiegoramirez098@gmail.com>
2023-08-22 17:08:16 -0500

    feat: :construction: avance de la version api

--- src/routes/v1/locales.js
+++ src/routes/v1/locales.js
@@ -1,25 +1,21 @@
 import { Router } from "express";
 import Locales from "../../services/locales.js"
 import { validate } from "../../validations/validateService.js";
-import routesVersioning  from 'express-routes-versioning';
+
 const router = Router()
 
-router.get('/:id', routesVersioning({
-    "^1.1.1": Locales.getLocal,
-}));
+router.post('/agregar', validate(Locales.postLocal))
 
-// router.post('/agregar', )
+router.get('/obtener', validate(Locales.getLocal))
 
-// router.get('/obtener', validate(Locales.getLocal))
+router.put('/actualizar/:id', validate(Locales.putLocal))
 
-// router.put('/actualizar/:id', validate(Locales.putLocal))
+router.delete('/eliminar/:id', validate(Locales.deleteLocal))
 
-// router.delete('/eliminar/:id', validate(Locales.deleteLocal))
+router.get('/obtener/:id', validate(Locales.getLocalById))
 
-// router.get('/obtener/:id', validate(Locales.getLocalById))
+router.post('/:id/producto', validate(Locales.postProductoLocal))
 
-// router.post('/:id/producto', validate(Locales.postProductoLocal))
-
-// router.get('/categorias', validate(Locales.getLocalesByCategory))
+router.get('/categorias', validate(Locales.getLocalesByCategory))
 
 export { router };
