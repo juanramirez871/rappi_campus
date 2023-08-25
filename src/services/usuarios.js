598a0aa4d085e4c20e98cc5ab717664375c82f74
JoseDanielNovaMuñozCAMPUS <josedanielnova2027@gmail.com>
2023-08-24 21:12:15 -0500

    feat: :sparkles: endpoint usuarios
    realice la consulta para que un usuario pueda agregar un pedido y eliminarlo

--- src/services/usuarios.js
+++ src/services/usuarios.js
@@ -50,4 +50,22 @@ export default class Usuarios {
 
         res.status(200).json({ productos, pedido: consultaPedidos });
     }
+    static async postUsuarioPedido(req, res){
+        const pedidos = db.getInstance().changeCollection('pedidos').connect();
+        let consulta = await pedidos.insertOne(req.body)
+        res.status(200).json(consulta)
+    }
+    static async deleteUsuarioPedido(req, res) {
+        const pedidos = db.getInstance().changeCollection('pedidos').connect();
+        try {
+            const consulta = await pedidos.updateOne({_id: new ObjectId(req.params.id),usuarioId: req.params.usuarioId},{$set: { activo: 0 }});
+            if (consulta.matchedCount === 1) {
+                res.status(200).json({ msg: "Pedido desactivado exitosamente" });
+            } else {
+                res.status(404).json({ msg: "Pedido no encontrado o no pertenece al usuario" });
+            }
+        } catch (error) {
+            res.status(500).json({ msg: "Error en el servidor" });
+        }
+    }
 }
